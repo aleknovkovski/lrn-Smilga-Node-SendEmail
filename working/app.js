0be2e4ee73708efb96e7ca9a155ed9de47a149fe
Alek Novkovski <alek@greatcode2.com>
2023-01-31 00:17:15 +0100

    Setup /send route

--- working/app.js
+++ working/app.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const sendEmail = require('./controllers/sendEmail')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -14,6 +15,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('<h1>Email Project</h1> <a href="/send">send email</a>');
 });
+
+app.get('/send', sendEmail)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
---
+++ working/controllers/sendEmail.js
@@ -0,0 +1,5 @@
+async function sendEmail (req, res) {
+    res.send('send email functionality')
+}
+
+module.exports = sendEmail
