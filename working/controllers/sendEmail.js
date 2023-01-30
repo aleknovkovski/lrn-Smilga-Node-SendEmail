9e9194697ddfcaa36df09da237e6dfd50e58e7f7
Alek Novkovski <alek@greatcode2.com>
2023-01-31 00:55:35 +0100

    Setup sending w nodemailer/ethereal

--- working/controllers/sendEmail.js
+++ working/controllers/sendEmail.js
@@ -1,4 +1,25 @@
+const nodemailer = require('nodemailer');
+
 async function sendEmail (req, res) {
+    let testAccount = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: process.env.TRANSPORTER_USER,
+            pass: process.env.TRANSPORTER_PASSWORD
+        }
+    });
+
+    let info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: "bar@example.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello zzzworld?", // plain text body
+        html: "<b>Hello worldzz?</b>", // html body
+    });
+
     res.send('send email functionality')
 }
 
