d2a0b5a42c5f935f18af609b145e5599a5482380
Alek Novkovski <alek@greatcode2.com>
2023-01-31 01:36:45 +0100

    Build out sendGrid testing controller

--- working/controllers/sendEmail.js
+++ working/controllers/sendEmail.js
@@ -25,19 +25,18 @@ async function sendEmailEth (req, res) {
 }
 
 async function sendEmailSG (req, res) {
-    const sgMail = require('@sendgrid/mail')
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     const msg = {
         to: process.env.TEST_RECIPIENT, // Change to your recipient
         from: process.env.VERIFIED_SG_SENDER, // Change to your verified sender
-        subject: 'Sending with SendGrid is Fun',
-        text: 'and easy to do anywhere, even with Node.js',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+        subject: 'Sending with SendGrid is Fun (Test)',
+        text: 'Just testing',
+        html: 'Just testing',
     }
     sgMail
         .send(msg)
-        .then(() => {
-            console.log('Email sent')
+        .then((wut) => {
+            console.log(wut)
         })
         .catch((error) => {
             console.error(error)
